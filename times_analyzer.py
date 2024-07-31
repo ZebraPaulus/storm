1f7a37f1a7d65142ae8262f0164c59eeb0086c58
David Grawe <david.grawe@mail.de>
2024-07-31 15:17:41 +0200

    Slight changes

--- times_analyzer.py
+++ times_analyzer.py
@@ -6,15 +6,18 @@ times = pd.read_csv('times.csv')
 # get time deltas
 for i in range(len(times.columns)-1,0,-1):
     times[times.columns[i]] = times[times.columns[i]]-times[times.columns[i-1]]
-# drop starting time
-times.drop(columns=[times.columns[0]], inplace=True)
+# set first column as sum of all other columns
+times[times.columns[0]] = 0
+times[times.columns[0]] = times.sum(axis=1)
 
 print(times)
 # filter out all rows with a 95% percentile high value in a column
+print("\nTiming overview:\n"+"="*60)
 for col in times.columns:
     if times[col].quantile(0.95) > 0.003:
-        print("[Percentiles]: ".rjust(18),col.ljust(15), times[col].quantile(0.99),",\t", times[col].quantile(0.95),",\t", times[col].quantile(0.75))
-        print("[Mean, Max, Min]: ",col.ljust(15), times[col].mean(), ",\t", times[col].max(), ",\t", times[col].min())
+        print("[Percentiles]: ".rjust(18)+"{}: {:.5f} | {:.5f} | {:.5f} |".format(col.ljust(11), times[col].quantile(0.99), times[col].quantile(0.95), times[col].quantile(0.75)))
+        print("[Mean, Max, Min]: {}: {:.5f} | {:.5f} | {:.5f} |".format(col.ljust(11), times[col].mean(), times[col].max(), times[col].min()))
+        print("-"*60)
 
 # make boxplots of the times
 plt.figure(figsize=(10,10))
